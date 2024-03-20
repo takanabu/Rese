f304b0d5ee4afb25971b6e28a546c63e130bd017
takayama manabu <mt01work01@gmail.com>
2024-03-20 22:31:42 +0900

    <ログインぺージ途中>

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Atte</title>
+    <title>Rese</title>
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,6 +19,20 @@
     </main>
     @component('components.footer')
     @endcomponent
+
+    <ul class="header-nav">
+        @if (Auth::check())
+        <li class="header-nav__item">
+            <a class="header-nav__link" href="/mypage">マイページ</a>
+        </li>
+        <li class="header-nav__item">
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button class="header-nav__button">ログアウト</button>
+            </form>
+        </li>
+        @endif
+    </ul>
 </body>
 
 </html>
