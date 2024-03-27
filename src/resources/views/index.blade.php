62616eaef7be8a52d601f030e4dc3bec7b486bed
takayama manabu <mt01work01@gmail.com>
2024-03-27 22:30:23 +0900

    <ショップ一覧ページ途中>

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,16 +1,28 @@
 @extends('layouts.app')
 
-@section('content')
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endpush
+
+@section('main')
 <div class="container">
-    @foreach ($shops as $shop)
-    <div class="card mb-4">
-        <img src="{{ $shop->image_url }}" class="card-img-top" alt="{{ $shop->shop_name }}">
-        <div class="card-body">
-            <h5 class="card-title">{{ $shop->shop_name }}</h5>
-            <p class="card-text">{{ $shop->shop_overview }}</p>
-            <a href="/shop/{{ $shop->id }}" class="btn btn-primary">詳しく見る</a>
+    <div class="row justify-content-center">
+        @foreach ($shops as $shop)
+        <div class="col-md-3 mb-4"> 
+            <div class="card shadow-sm">
+                <img src="{{ $shop->image_url }}" alt="{{ $shop->shop_name }}" class="bd-placeholder-img card-img-top" width="100%" height="225">
+                <div class="card-body">
+                    <p class="card-text"><strong>{{ $shop->shop_name }}</strong></p> <!-- ショップ名を太字にします -->
+                    <small class="text-muted">#{{ $shop->region }} #{{ $shop->genre }}</small> <!-- リージョンとジャンルの前に#を追加します -->
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='/detail/{{ $shop->id }}'">詳しく見る</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
 @endsection
