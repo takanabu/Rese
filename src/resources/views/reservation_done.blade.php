109c91a0ffd2e6facc6b3b96bacfa3be5e6c5fe4
takayama manabu <mt01work01@gmail.com>
2024-04-06 13:54:32 +0900

    <ショップ詳細ページ予約登録機能途中>

---
+++ src/resources/views/reservation_done.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/reservation_done.css') }}">
+@endpush
+
+@section('main')
+<div class="done-container">
+    <div class="card-body">
+        <h2 class="text-center">ご予約ありがとうございます</h2>
+
+        @if (session('result'))
+        <div class="flash_message">
+            {{ session('result') }}
+        </div>
+        @endif
+
+        <div class="form-group">
+            <a href="{{ route('login') }}" class="thanks-btn">
+                {{ __('戻る') }}
+            </a>
+        </div>
+    </div>
+</div>
+@endsection
